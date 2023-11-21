3a9f857c0b120288aacd47584f3ddbc2f4eed726
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-21 18:59:41 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: "は全角文字のみで入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: "は全角文字のみで入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
   validates :birth_day, presence: true
 end
