337dbd02701b4376ef42c3fe2cb6e8884dca30d6
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-21 17:19:39 +0900

    userモデルにバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  varidates :nickname, presence: true
+  varidates :first_name, presence: true
+  varidates :last_name, presence: true
+  varidates :first_name_kana, presence: true
+  varidates :last_name_kana, presence: true
+  varidates :birth_day, presence: true
 end
