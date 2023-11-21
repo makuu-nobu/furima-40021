a6ae26131fae219296a83dbdcc4545111b56ae2c
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-21 17:45:51 +0900

    ログイン・ログアウト機能を実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@gmail.com', password: '111aaa', )
+    end
+  end
 end
