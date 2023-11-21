dba7017126cf1bf22402bc7edfd7392f2b34eaa6
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-21 18:00:03 +0900

    Factory_botの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,18 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = User.new(nickname: '', email: 'test@gmail.com', password: '111aaa', )
+      user = User.new(nickname: '', email: 'test@gmail.com', password: '111aaa', password_confirmation: '111aaa',
+                      first_name: '田中', last_name:'太郎', first_name_kana: 'タナカ', last_name_kana: 'タロウ',
+                      birth_day: '2000-01-01')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      user = User.new(nickname: 'タナカ', email: '', password: '111aaa', password_confirmation: '111aaa',
+                      first_name: '田中', last_name:'太郎', first_name_kana: 'タナカ', last_name_kana: 'タロウ',
+                      birth_day: '2000-01-01')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
     end
   end
 end
