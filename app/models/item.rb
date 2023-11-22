1860f9c465b204e53aa848efeab5151a82ee8223
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-22 15:44:43 +0900

    itemモデルの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+    validates :name, presence: true
+    validates :content, presence: true
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+    validates :situation_id, numericality: { other_than: 1, message: "can't be blank" } 
+    validates :charge_id, numericality: { other_than: 1, message: "can't be blank" } 
+    validates :region_id, numericality: { other_than: 1, message: "can't be blank" } 
+    validates :how_long_id, numericality: { other_than: 1, message: "can't be blank" } 
+    validates :price, presence: true
+
+    belongs_to :user
+    has_one_attached :image
 end
