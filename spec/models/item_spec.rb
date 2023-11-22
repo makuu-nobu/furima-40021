89e5033ded34189d9949e709327558a05724c5ba
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-22 18:46:15 +0900

    item用のFactory_botの作成と正常時のテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '商品出品機能' do
+    context '商品出品ができる時' do
+      it '必要情報がすべて記載されていれば商品が出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+  end
 end
