67d5b000cdb96e74b9fa2622bcb59e70f9692a9b
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-23 16:44:11 +0900

    indexビューの調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
     before_action :move_to_session, except: [:index, :show]
     def index
-        
+        @items = Item.all.order("created_at DESC")        
     end
 
     def new
