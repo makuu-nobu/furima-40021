71354fb1b30b3e7c82b55b51b6a540c620c3c234
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-23 11:40:25 +0900

    インデントの調整

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,20 +3,19 @@ const addTaxPriceElement = document.getElementById('add-tax-price');
 const profitElement = document.getElementById('profit');
 
 function calculateValues() {
-const price = parseFloat(priceInput.value);
+    const price = parseFloat(priceInput.value);
 
-if (!isNaN(price)) {
-    const taxPrice = price * 0.1;
-    addTaxPriceElement.textContent = taxPrice.toFixed();
+    if (!isNaN(price)) {
+        const taxPrice = price * 0.1;
+        addTaxPriceElement.textContent = taxPrice.toFixed();
 
-    const profit = price - taxPrice;
-    profitElement.textContent = profit.toFixed();
-} else {
-    addTaxPriceElement.textContent = "";
-    profitElement.textContent = "";
+        const profit = price - taxPrice;
+        profitElement.textContent = profit.toFixed();
+    } else {
+        addTaxPriceElement.textContent = "";
+        profitElement.textContent = "";
+    }
 }
-}
-
 // 初回の計算
 calculateValues();
 
