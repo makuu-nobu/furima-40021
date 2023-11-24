e16d8aeb4dc7dbd54b95449bfb1172f1c56a9c99
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-24 16:13:58 +0900

    purchaseモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,5 +24,5 @@ class Item < ApplicationRecord
 
     belongs_to :user
     has_one_attached :image
-    
+    has_one :purchase    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ApplicationRecord
   validates :birth_day, presence: true
 
   has_many :items
+  has_many :purchases
 end
