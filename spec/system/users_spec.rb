aa76467a63d6886a82dc15a78f015f6f7ced78e2
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-26 16:18:14 +0900

    ユーザーサインイン正常動作

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe 'ユーザー新規登録', type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.build(:user)
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ユーザー新規登録できる時' do
+    it '正しい情報を入力すればユーザ新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'nickname', with: @user.nickname
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      fill_in 'password-confirmation', with: @user.password_confirmation
+      fill_in 'last-name', with: @user.first_name
+      fill_in 'first-name', with: @user.last_name
+      fill_in 'last-name-kana', with: @user.first_name_kana
+      fill_in 'first-name-kana', with: @user.last_name_kana
+      select '1930', from: 'user[birth_day(1i)]'
+      select '10', from: 'user[birth_day(2i)]'
+      select '10', from: 'user[birth_day(3i)]'
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change{ User.count }.by(1)
+      # トップページへ遷移することを確認する
+      expect(page).to have_current_path(root_path)
+      # ページにログアウトボタンとユーザー名が現れていること
+      expect(page).to have_content('ログアウト')
+      expect(page).to have_content("#{@user.nickname}")
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      # 新規登録ページへ移動する
+      # ユーザー情報を入力する
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      # 新規登録ページへ戻されることを確認する
+    end
+  end
 end
